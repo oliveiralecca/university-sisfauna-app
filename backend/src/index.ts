482cc8e5c0ae0546018d800dc83cc8b7a5cd0aff
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-06-24 04:50:15 -0300

    feat(backend): initiated the application

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.info('Aplicação rodando em http://localhost:3333')
+});
