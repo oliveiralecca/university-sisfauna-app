53368f1088fc0c215d975a2679b61ec4ad9aaed8
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-06-24 19:00:19 -0300

    feat(backend): created new routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,17 @@ import RelatorioController from "./controllers/RelatorioController";
 
 const routes = Router();
 
-routes.get('/', RelatorioController.getSergipe);
+const BASE_URL_V1 = '/api/v1';
+
+routes.get(`${BASE_URL_V1}/sergipe`, RelatorioController.getCountSergipe);
+routes.get(`${BASE_URL_V1}/sergipe/filtro`, RelatorioController.getCountSergipeByPeriod);
+
+routes.get(`${BASE_URL_V1}/ativas`, RelatorioController.getCountActive);
+
+routes.get(`${BASE_URL_V1}/classes`, RelatorioController.getClasses);
+
+routes.get(`${BASE_URL_V1}/estados`, RelatorioController.getEstados);
+
+routes.get(`${BASE_URL_V1}/municipios`, RelatorioController.getMunicipios);
 
 export default routes;
