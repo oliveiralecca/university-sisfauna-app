8c6e565002e6685a9b201f09acb7e223db2b3aae
Marcos Paulo <marcos.paullo32@gmail.com>
2023-06-26 02:13:12 -0300

    feat: add data in answers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,22 @@ import reactLogo from "./assets/react.svg";
 
 import "./App.css";
 import Questions from "./components/Questions/Questions";
+import { useFetch } from "./hooks/useFetch";
+import Answers from "./components/Answers/Answers";
+
 
 function App() {
   const [count, setCount] = useState(0);
+  const { data: classes } = useFetch<string[]>("http://localhost:3333/api/v1/classes");
+  const { data: ativas } = useFetch<string[]>("http://localhost:3333/api/v1/ativas");
+  const { data: sergipe } = useFetch<string[]>("http://localhost:3333/api/v1/sergipe");
+  //const { data: municipio } = useFetch<string[]>("http://localhost:3333/api/v1/municipios");
+  const { data: estados } = useFetch<string[]>("http://localhost:3333/api/v1/estados");
+  const { data: nomePopulares } = useFetch<string[]>("http://localhost:3333/api/v1/nomespopulares");
+  //const { data: atividade } = useFetch<string[]>("http://localhost:3333/api/v1/atividade");
+  //const { data: detalhes } = useFetch<string[]>("http://localhost:3333/api/v1/detalhes");
+  //const { data: obitos } = useFetch<string[]>("http://localhost:3333/api/v1/obitos");
+  //const { data: ordemAnimal } = useFetch<string[]>("http://localhost:3333/api/v1/ordemanimal");
 
   return (
     <>
@@ -17,64 +30,67 @@ function App() {
       </div>
       <h1>University Sisfauna App</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <Questions
           pergunta="1º Quantas pessoas jurídicas entregaram o Relatório de Atividades Potencialmente
 Poluidoras (RAPP) no estado de Sergipe?"
-          resposta="aassasas"
         />
+        <Answers resposta={sergipe} />
+
         <Questions
           pergunta="2º Qual a quantidade da Situação Cadastral Ativa?
 "
-          resposta="aassasas"
         />
+        <Answers resposta={ativas} />
         <Questions
-          pergunta="3º Apresente quais são as Classes dos animais presentes no BD?
-"
-          resposta="aassasas"
+          pergunta="3º Apresente quais são as Classes dos animais presentes no BD?"
         />
+        <Answers resposta={classes} />
         <Questions
           pergunta="4º Quais os Estados que fazem parte do Sisfauna?
 "
-          resposta="aassasas"
         />
+        <Answers resposta={estados} />
         <Questions
           pergunta="5° Liste todos os Municípios presentes no banco de dados, e a quantidade total dos
           mesmos.
 
 "
-          resposta="aassasas"
         />
+        {/* <Answers resposta={console.log(municipios)}/> */}
+        
+
         <Questions
           pergunta="6º Liste o nome popular de todos os animais presentes na Sisfauna.
 
 "
-          resposta="aassasas"
         />
+        <Answers  resposta={nomePopulares}/>
         <Questions
           pergunta="7º Qual a Categoria de Atividade que mais se repete e o total dessa Categoria?
 
 "
-          resposta="aassasas"
         />
+        
         <Questions
           pergunta="8º Quais são os detalhes informados?
 
 "
-          resposta="aassasas"
         />
+        
+       
+
         <Questions
           pergunta="9º Qual o nome, ou os nomes Populares dos animais que tiveram óbitos igual a 50?
 "
-          resposta="aassasas"
         />
+        
         <Questions
           pergunta="10º Qual a Ordem de animal que mais se repete e o total deste item?
 "
-          resposta="aassasas"
         />
+        
+        
       </div>
     </>
   );
