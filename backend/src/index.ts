33b6766b49c49f7f68a4b6ba701e76f69ec91776
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-06-27 23:20:12 -0300

    refactor(backend): updated the code to deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,25 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
-const port = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3333;
+
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
-  console.info('Aplicação rodando em http://localhost:3333')
-});
+try {
+  prisma.$connect();
+  app.listen(PORT, () => {
+    console.info('Aplicação rodando em http://localhost:3333')
+  });
+} catch (e) {
+  console.error(e);
+}
+
+
