f13d6fe1366ada1af63bf2a4fb002e82968f7908
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-08-16 23:22:48 -0300

    test(backend): set a test origin to cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,8 @@ const prisma = new PrismaClient();
 
 app.use(
   cors({
-    origin: "https://sisfauna-app.vercel.app/",
+    // origin: "https://sisfauna-app.vercel.app/",
+    origin: "http://localhost:5173",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
