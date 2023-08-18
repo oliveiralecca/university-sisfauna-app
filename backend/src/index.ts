37334de68e2bb07d00a63af07b2c61e6efc7ca6b
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-08-18 12:45:29 -0300

    refactor(backend): changed origin on cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const prisma = new PrismaClient();
 app.options("*", cors());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://sisfauna-app.vercel.app",
     allowedHeaders: [
       "Content-Type",
       "Authorization",
