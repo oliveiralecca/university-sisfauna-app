f6f6794ed8790cca87eb3e6414485b0d918efbd9
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-08-20 21:05:24 -0300

    feat(backend): created a routine to be executed every 6th month

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
 import { routes } from "./routes";
 import { PrismaClient } from "@prisma/client";
+import cron from "node-cron";
+import { Agent } from "https";
 
 const app = express();
 
@@ -9,10 +12,9 @@ const PORT = process.env.PORT || 3333;
 
 const prisma = new PrismaClient();
 
-app.options("*", cors());
 app.use(
   cors({
-    origin: "https://sisfauna-app.vercel.app",
+    origin: "*",
     allowedHeaders: [
       "Content-Type",
       "Authorization",
@@ -30,6 +32,149 @@ app.use(
 app.use(express.json());
 app.use(routes);
 
+async function getNewData() {
+  const agent = new Agent({
+    rejectUnauthorized: false,
+  });
+
+  try {
+    const newData: any = await axios
+      .get(
+        "https://dadosabertos.ibama.gov.br/dados/RAPP/sisfaunaPlantelExato/relatorio.json",
+        { httpsAgent: agent }
+      )
+      .then((res: any) => res.data);
+
+    if (newData.data) {
+      for (const item of newData.data) {
+        const itemExists = await prisma.reports.findFirst({
+          where: {
+            abates: Number(item.abates.replace(",", ".")),
+            ano: Number(item.ano),
+            aquisicoes: Number(item.aquisicoes.replace(",", ".")),
+            ate: item.ate,
+            categoria_de_atividade: item.categoriaAtividade,
+            classe: item.classe,
+            cnpj: item.cnpj,
+            codigo_da_categoria: Number(item.codigoCategoria),
+            codigo_do_detalhe: Number(item.codigoDetalhe),
+            detalhe: item.detalhe,
+            estado: item.estado,
+            evasoes: Number(item.evasoes.replace(",", ".")),
+            femeas_plantel_anterior: Number(
+              item.femeasPlantelAnterior.replace(",", ".")
+            ),
+            femeas_plantel_atual: Number(
+              item.femeasPlantelAtual.replace(",", ".")
+            ),
+            furtos_roubos: Number(item.furtosRoubos.replace(",", ".")),
+            indeterminado_plantel_anterior: Number(
+              item.IndeterminadoPlantelAnterior.replace(",", ".")
+            ),
+            indeterminado_plantel_atual: Number(
+              item.IndeterminadoPlantelAtual.replace(",", ".")
+            ),
+            machos_plantel_anterior: Number(
+              item.machosPlantelAnterior.replace(",", ".")
+            ),
+            machos_plantel_atual: Number(
+              item.machosPlantelAtual.replace(",", ".")
+            ),
+            municipio: item.municipio,
+            nascimentos: Number(item.nascimentos.replace(",", ".")),
+            nome_cientifico: item.nomeCientifico,
+            nome_popular: item.nomePopular ? item.nomePopular : "não consta",
+            obitos: Number(item.obitos.replace(",", ".")),
+            ordem: item.ordem,
+            periodo: item.periodo,
+            razao_social: item.razaoSocial,
+            reintegracao_solturas: Number(item.reintSolturas.replace(",", ".")),
+            retificadora: item.retificadora,
+            situacao_cadastral: item.situacaoCadastral,
+            total: Number(item.total.replace(",", ".")),
+            total_de_entradas: Number(item.totalEntradas.replace(",", ".")),
+            total_de_saidas: Number(item.totalSaidas.replace(",", ".")),
+            total_geral: Number(item.totalGeral.replace(",", ".")),
+            transferencia_entradas: Number(
+              item.transfEntradas.replace(",", ".")
+            ),
+            transferencia_saidas: Number(item.transSaidas.replace(",", ".")),
+            vendas: Number(item.vendas.replace(",", ".")),
+          },
+        });
+
+        if (!itemExists) {
+          await prisma.reports.create({
+            data: {
+              abates: Number(item.abates.replace(",", ".")),
+              ano: Number(item.ano),
+              aquisicoes: Number(item.aquisicoes.replace(",", ".")),
+              ate: item.ate,
+              categoria_de_atividade: item.categoriaAtividade,
+              classe: item.classe,
+              cnpj: item.cnpj,
+              codigo_da_categoria: Number(item.codigoCategoria),
+              codigo_do_detalhe: Number(item.codigoDetalhe),
+              detalhe: item.detalhe,
+              estado: item.estado,
+              evasoes: Number(item.evasoes.replace(",", ".")),
+              femeas_plantel_anterior: Number(
+                item.femeasPlantelAnterior.replace(",", ".")
+              ),
+              femeas_plantel_atual: Number(
+                item.femeasPlantelAtual.replace(",", ".")
+              ),
+              furtos_roubos: Number(item.furtosRoubos.replace(",", ".")),
+              indeterminado_plantel_anterior: Number(
+                item.IndeterminadoPlantelAnterior.replace(",", ".")
+              ),
+              indeterminado_plantel_atual: Number(
+                item.IndeterminadoPlantelAtual.replace(",", ".")
+              ),
+              machos_plantel_anterior: Number(
+                item.machosPlantelAnterior.replace(",", ".")
+              ),
+              machos_plantel_atual: Number(
+                item.machosPlantelAtual.replace(",", ".")
+              ),
+              municipio: item.municipio,
+              nascimentos: Number(item.nascimentos.replace(",", ".")),
+              nome_cientifico: item.nomeCientifico,
+              nome_popular: item.nomePopular ? item.nomePopular : "não consta",
+              obitos: Number(item.obitos.replace(",", ".")),
+              ordem: item.ordem,
+              periodo: item.periodo,
+              razao_social: item.razaoSocial,
+              reintegracao_solturas: Number(
+                item.reintSolturas.replace(",", ".")
+              ),
+              retificadora: item.retificadora,
+              situacao_cadastral: item.situacaoCadastral,
+              total: Number(item.total.replace(",", ".")),
+              total_de_entradas: Number(item.totalEntradas.replace(",", ".")),
+              total_de_saidas: Number(item.totalSaidas.replace(",", ".")),
+              total_geral: Number(item.totalGeral.replace(",", ".")),
+              transferencia_entradas: Number(
+                item.transfEntradas.replace(",", ".")
+              ),
+              transferencia_saidas: Number(item.transSaidas.replace(",", ".")),
+              vendas: Number(item.vendas.replace(",", ".")),
+            },
+          });
+        }
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+const updateData = () => {
+  cron.schedule("0 0 1 */6 *", () => getNewData());
+};
+
 const connectDB = async () => {
   try {
     await prisma.$connect();
@@ -40,6 +185,8 @@ const connectDB = async () => {
 };
 
 connectDB().then(() => {
+  updateData();
+
   app.listen(PORT, () => {
     console.info(`Aplicação rodando na porta ${PORT}`);
   });
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,36 +6,88 @@ import { AuthController } from "./controllers/Auth";
 
 const routes = Router();
 
-const BASE_URL_V2 = '/api/v2';
+const BASE_URL_V2 = "/api/v2";
 
 /* V2 Routes -> with authentication */
 routes.post(`${BASE_URL_V2}/location`, LocationController.postClientLocation);
-routes.get(`${BASE_URL_V2}/locations`, AuthController.checkToken, LocationController.getAllLocations);
+routes.get(
+  `${BASE_URL_V2}/locations`,
+  AuthController.checkToken,
+  LocationController.getAllLocations
+);
 
 routes.post(`${BASE_URL_V2}/register`, AuthController.createUser);
 routes.post(`${BASE_URL_V2}/login`, AuthController.userLogin);
 
-routes.get(`${BASE_URL_V2}/estados`, AuthController.checkToken, ReportController.getStates);
-routes.get(`${BASE_URL_V2}/anos`, AuthController.checkToken, ReportController.getYears);
+routes.get(
+  `${BASE_URL_V2}/estados`,
+  AuthController.checkToken,
+  ReportController.getStates
+);
+routes.get(
+  `${BASE_URL_V2}/anos`,
+  AuthController.checkToken,
+  ReportController.getYears
+);
 
-routes.get(`${BASE_URL_V2}/entregarelatorio`, AuthController.checkToken, ReportController. getCountReportDelivery);
+routes.get(
+  `${BASE_URL_V2}/entregarelatorio`,
+  AuthController.checkToken,
+  ReportController.getCountReportDelivery
+);
 
-routes.get(`${BASE_URL_V2}/ativas`, AuthController.checkToken, ReportController.getCountActive);
+routes.get(
+  `${BASE_URL_V2}/ativas`,
+  AuthController.checkToken,
+  ReportController.getCountActive
+);
 
-routes.get(`${BASE_URL_V2}/classes`, AuthController.checkToken, ReportController.getClasses);
+routes.get(
+  `${BASE_URL_V2}/classes`,
+  AuthController.checkToken,
+  ReportController.getClasses
+);
 
-routes.get(`${BASE_URL_V2}/nascimentos`, AuthController.checkToken, ReportController.getBirths);
+routes.get(
+  `${BASE_URL_V2}/nascimentos`,
+  AuthController.checkToken,
+  ReportController.getBirths
+);
 
-routes.get(`${BASE_URL_V2}/municipios`, AuthController.checkToken, ReportController.getCities);
+routes.get(
+  `${BASE_URL_V2}/municipios`,
+  AuthController.checkToken,
+  ReportController.getCities
+);
 
-routes.get(`${BASE_URL_V2}/aquisicoes`, AuthController.checkToken, ReportController.getAcquired);
+routes.get(
+  `${BASE_URL_V2}/aquisicoes`,
+  AuthController.checkToken,
+  ReportController.getAcquired
+);
 
-routes.get(`${BASE_URL_V2}/repteis`, AuthController.checkToken, ReportController.getReptiles);
+routes.get(
+  `${BASE_URL_V2}/repteis`,
+  AuthController.checkToken,
+  ReportController.getReptiles
+);
 
-routes.get(`${BASE_URL_V2}/furtados`, AuthController.checkToken, ReportController.getStolen);
+routes.get(
+  `${BASE_URL_V2}/furtados`,
+  AuthController.checkToken,
+  ReportController.getStolen
+);
 
-routes.get(`${BASE_URL_V2}/obitos`, AuthController.checkToken, ReportController.getDeaths);
+routes.get(
+  `${BASE_URL_V2}/obitos`,
+  AuthController.checkToken,
+  ReportController.getDeaths
+);
 
-routes.get(`${BASE_URL_V2}/ordemanimal`, AuthController.checkToken, ReportController.getAnimalOrder);
+routes.get(
+  `${BASE_URL_V2}/ordemanimal`,
+  AuthController.checkToken,
+  ReportController.getAnimalOrder
+);
 
 export { routes };
